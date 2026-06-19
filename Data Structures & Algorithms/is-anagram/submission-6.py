995b31615a4d7d952d9cb42e1c7f7e031fc1e4a0
Roshan Raj <112398277+Roshan3580@users.noreply.github.com>
2026-06-19 14:29:48 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        set1 = dict()
+        set2 = dict()
+        for char1 in s:
+            if char1 in set1:
+                set1[char1] += 1
+            else:
+                set1[char1] = 1
+        for char2 in t:
+            if char2 in set2:
+                set2[char2] += 1
+            else:
+                set2[char2] = 1
+        
+        return set1 == set2
